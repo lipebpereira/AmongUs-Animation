14a8edcda69adb7bdc9cdc272c655813c3405420
Felipe <felipe.p@aluno.ufabc.edu.br>
2021-03-24 18:03:51 -0300

    Segundo commit

--- animation.js
+++ animation.js
@@ -1,8 +1,21 @@
 const width = 93;
 const height = 122;
-const frames = 11;
+var playerFrames = 11;
 
 window.onload = () => {
     canvas = document.createElement('canvas');
     document.body.appendChild(canvas);
+    ctx = canvas.getContext('2d');
+
+    spritesheet = new Image();
+    spritesheet.src = 'spritesheet.png';
+    frames = 0;
+    playerFrames = 0;
+    spritesheet.onload = animate;
+}
+
+const animate = () => {
+    frames++;
+    console.log(frames);
+    window.requestAnimationFrame(animate);
 }
